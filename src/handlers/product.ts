99ffb4a0c723b8942e9300f94167fdf8f455d87e
danielcontt987 <josedanielcontrerasperez510@gmial.com>
2025-05-19 21:16:22 -0700

    validate name

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,13 +1,27 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import Product from "../models/Product.model";
+import { check, validationResult } from "express-validator";
+
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+
+    //Validación
+    await check("name").notEmpty().withMessage('El nombre del producto no puede ir vació').run(req);
+
+    // let errors = validationResult(req)
+
+        let errors = validationResult(req);
+       
+        if (!errors.isEmpty()) {
+          res.status(400).json({ errors: errors.array() });
+          return;
+        }
+       
+       const saveProduct = await Product.create(req.body);
+       res.json({
+           data: saveProduct,
+           status: 201
+       }).status(201)
+      };
 
 
-
-export const createProduct = async (req: Request, res: Response) => {
-    const product = new Product(req.body);
-    const saveProduct = await product.save();
-    res.json({
-        data: saveProduct,
-        status: 201
-    }).status(201)
-}
+// }
